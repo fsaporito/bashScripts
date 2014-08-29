1d9b38fbd5e8fc0fda28a11ac203d77cc400de38
Francesco Saporito <sapo93@gmail.com>
2014-08-29 15:01:12 +0200

    Update myGit.sh

--- System/myGit.sh
+++ System/myGit.sh
@@ -7,8 +7,10 @@ Bash="https://github.com/sapo93/bashScripts.git"
 Dijkstra="https://github.com/sapo93/dijkstralog.git"
 Euler="https://github.com/sapo93/EulerOdeSolver.git"
 JADS="https://github.com/sapo93/JADS.git"
+mathParser="https://github.com/sapo93/mathParser.git"
 numC="https://github.com/sapo93/numC.git"
 numDiploma="https://github.com/sapo93/num_diploma.git"
+Octave="https://github.com/UniversityProjects/OctaveMatlab.git"
 OOL="https://github.com/sapo93/OOL.git"
 Parallel="https://github.com/sapo93/ParallelRandomGenerator.git"
 TicTacToe="https://github.com/sapo93/TicTacToe.git"
@@ -17,6 +19,7 @@ TicTacToe="https://github.com/sapo93/TicTacToe.git"
 #University
 Algo="https://github.com/UniversityProjects/Algorithms.git"
 Dist="https://github.com/UniversityProjects/DistributedSystems.git"
+Prolog="https://github.com/UniversityProjects/Prolog.git"
 ProgI="https://github.com/UniversityProjects/ProgrammingI.git"
 ProgII="https://github.com/UniversityProjects/ProgrammingII.git"
 
@@ -36,8 +39,10 @@ git clone ${Bash}
 git clone ${Dijkstra}
 git clone ${Euler}
 git clone ${JADS}
+git clone ${mathParser}
 git clone ${numC}
 git clone ${numDiploma}
+git clone ${Octave}
 git clone ${OOL}
 git clone ${Parallel}
 git clone ${TicTacToe}
