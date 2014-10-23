6025b3bd1443f5cfda0190cc4ff2e2495877d284
Francesco Saporito <sapo93@gmail.com>
2014-10-23 21:44:04 +0200

    Update gitUpdate.sh

--- System/gitUpdate.sh
+++ System/gitUpdate.sh
@@ -2,11 +2,20 @@
 
 clear
 
-until test -d "$DIR"
+DIR = ""
+
+
+until test -d "${DIR}"
 do
 	
 	echo -n "Input The GitHub Directory: "
 	read DIR
+	
+	if [ -nd ${DIR}]; then
+		
+		echo "Error: Not A Directory!!!"
+		
+	fi
 	
 done
 	
@@ -17,13 +26,55 @@ cd $DIR
 for i in *
 do
 
-	echo "$i"
+	echo "${i}"
 	
-	if [ -d $i ]; then
-	
-		cd $i
-		git pull
-		cd ..
+	if [ -d "${i}" ]; then
+
+		cd ${i}
+		ls
+		cho ""
+		echo ""
+
+		if [ "${i}" == "UniversityProjects" ]; then
+		 			
+			for j in *
+			do	
+				
+				if [ -d "${j}" ]; then
+					
+					echo "University: ${j}"	
+					cd ${j}
+					ls
+					echo ""
+					echo ""
+					git pull
+					
+					if [ "${i}" == "2014-15" ]; then
+					
+						cd karp-rabin
+						cd miller-rabin
+						git submodule update
+						cd ..
+						cd klib
+						git submodule update
+						cd ..
+						cd ..
+					
+					fi
+
+					cd ..
+
+				fi
+
+			done
+
+		else		
+
+			git pull
+			cd ..
+
+		fi
+
 	fi
 	
 done
